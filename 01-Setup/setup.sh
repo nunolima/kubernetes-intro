2b7ac609075f6c2d88ec2f44ed611af491a738eb
Nuno Lima <nuno.lima@jscrambler.com>
2019-12-12 17:18:39 +0000

    setup: added more steps

--- 01-Setup/setup.sh
+++ 01-Setup/setup.sh
@@ -77,4 +77,39 @@ kubectl version
 
 echo '============================='
 
+minikube start
+eval $(minikube docker-env)
+
+echo '============================='
+
+kubectl run hello-minikube --image=gcr.io/google_containers/echoserver:1.4 --port=8080
+kubectl get pods
+
+for i in {1..150}; do # timeout for 5 minutes
+  kubectl get pods 2>&1 | grep 'Running' &> /dev/null
+  if [ $? -ne 1 ]; then
+    break
+  fi
+  printf '.'
+  sleep 2
+done
+
+echo
+kubectl get pods
+
+# expose the pod’s port
+kubectl expose deployment hello-minikube --type=NodePort
+
+# get the URL of the ‘hello-minikube’ pod
+minikube service hello-minikube --url
+curl `minikube service hello-minikube --url`
+
+# check the IP address and open ports of all the Minikube pods
+kubectl get services
+
+echo '============================='
+
+minikube status && \
+#	minikube update-context
+	minikube dashboard
 
