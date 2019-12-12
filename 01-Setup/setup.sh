62a1d9b153c10bf7f6f575a8345992346914ea89
Nuno Lima <nuno.lima@jscrambler.com>
2019-12-12 16:42:06 +0000

    setup: updated docker-compose install steps

--- 01-Setup/setup.sh
+++ 01-Setup/setup.sh
@@ -40,18 +40,24 @@ sudo add-apt-repository \
    $(lsb_release -cs) \
    stable"
 sudo apt-get update && \
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 sudo apt-mark hold docker-ce docker-ce-cli containerd.io
 sudo groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker
+mkdir -p /home/"$USER"/.docker
 sudo chown "$USER":"$USER" /home/"$USER"/.docker -R
 sudo chmod g+rwx "$HOME/.docker" -R
 sudo systemctl enable docker
 #sudo netstat -lntp | grep dockerd
 
 # docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 sudo curl -L https://raw.githubusercontent.com/docker/compose/1.25.0/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
+source "$HOME/.bashrc"
+docker-compose --version
 
 # minkube
 sudo curl -L https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 -o /usr/local/bin/minikube
